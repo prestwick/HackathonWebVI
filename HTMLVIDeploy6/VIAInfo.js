0af418d55dae2f77c67bbaddd495ad1afe0c8f25
Mark Black <mark.prestwick@gmail.com>
2015-06-05 14:03:07 -0500

    Init
    Init

---
+++ HTMLVIDeploy6/VIAInfo.js
@@ -0,0 +1,10 @@
+//  via info  
+NationalInstruments.HtmlVI.viName = '_%46unction%2Eghtmlvi';
+
+//inputs: 
+NationalInstruments.HtmlVI.inputDataItem = {};
+
+//outputs:
+NationalInstruments.HtmlVI.outputDataItem = {};
+NationalInstruments.HtmlVI.outputDataItem['di_76']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_132']='String';
